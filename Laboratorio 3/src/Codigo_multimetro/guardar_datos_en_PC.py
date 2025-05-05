df367fd5e2afd3b254601f07f708240747e1dd6b
Jose Mario Navarro Bejarano <josemario.navarro@ucr.ac.cr>
2025-05-04 21:52:40 -0600

    Se agregan datos al reporte

--- Laboratorio 3/src/Codigo_multimetro/guardar_datos_en_PC.py
+++ Laboratorio 3/src/Codigo_multimetro/guardar_datos_en_PC.py
@@ -5,11 +5,12 @@ puerto_serial = serial.Serial('COM3', 9600, timeout=1)
 
 while True:
     """ if puerto_serial.in_waiting > 0:"""
-    voltajes = puerto_serial.readline().decode('utf-8').rstrip()  
+    voltajes = puerto_serial.readline().decode('utf-8').rstrip()
     print(voltajes + "\n")
     voltajes = voltajes.split()
-    
-    with open('reg_datos.csv', mode='a', newline='') as reg_datos:
-        
+
+    with open('./reg_datos.csv', mode='a', newline='') as reg_datos:
+
         writer = csv.writer(reg_datos)
         writer.writerow(voltajes)
+        
